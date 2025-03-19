dd58173c50fadb6c309688c016ccde9e65185023
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-03-19 16:30:55 +0530

    + setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import BudgetSlider from "@/components/BudgetSlider";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen w-[90%] mx-auto">
+      <BudgetSlider />
+    </main>
+  );
+}
